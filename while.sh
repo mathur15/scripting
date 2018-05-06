dc2c1bd38b1c4471856c029fb707129b3d5aada8
SomMathur <som.mathur@mail.utoronto.ca>
2018-05-06 02:13:11 -0400

    while loop script

---
+++ while.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Enter a number between 0 and 10"
+read -p "Input" number
+echo The number you entered is $number
+counter=1
+while [$counter -le 10]
+do
+	echo $number
+	((counter++))
+done
